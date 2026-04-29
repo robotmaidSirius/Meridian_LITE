216247c4e6341feebfee9451cc1fcfa8b1801ef3
TenrouHoshina <masiro.to.akari@gmail.com>
2026-04-29 20:07:54 +0900

    列挙型を移動

--- Meridian_LITE_for_ESP32/src/main.h
+++ Meridian_LITE_for_ESP32/src/main.h
@@ -10,84 +10,6 @@
 #include <MPU6050_6Axis_MotionApps20.h> // MPU6050用
 #include <Meridian.h>                   // Meridianのライブラリ導入
 
-//------------------------------------------------------------------------------------
-//  列挙型
-//------------------------------------------------------------------------------------
-
-enum ServoType { // サーボプロトコルのタイプ
-  NOSERVO = 0,   // サーボなし
-  PWM_S = 1,     // Single PWM (WIP)
-  PCA9685 = 11,  // I2C_PCA9685 to PWM (WIP)
-  FTBRSX = 21,   // FUTABA_RSxTTL (WIP)
-  DXL1 = 31,     // DYNAMIXEL 1.0 (WIP)
-  DXL2 = 32,     // DYNAMIXEL 2.0 (WIP)
-  KOICS3 = 43,   // KONDO_ICS 3.5 / 3.6
-  KOPMX = 44,    // KONDO_PMX (WIP)
-  JRXBUS = 51,   // JRPROPO_XBUS (WIP)
-  FTCSTS = 61,   // FEETECH_STS (WIP)
-  FTCSCS = 62    // FEETECH_SCS (WIP)
-};
-
-enum PadButton {  // リモコンボタンの列挙型
-  PAD_SELECT = 1, // Select
-  PAD_HOME = 2,   // HOME
-  PAD_L3 = 2,     // L3
-  PAD_R3 = 4,     // L4
-  PAD_START = 8,  // Start
-  PAD_UP = 16,    // 十字上
-  PAD_RIGHT = 32, // 十字右
-  PAD_DOWN = 64,  // 十字下
-  PAD_LEFT = 128, // 十字左
-  PAD_L2 = 256,   // L2
-  PAD_R2 = 512,   // R2
-  PAD_L1 = 1024,  // L1
-  PAD_R1 = 2048,  // R1
-  PAD_bU = 4096,  // △ 上
-  PAD_bR = 8192,  // o 右
-  PAD_bD = 16384, // x 下
-  PAD_bL = 32768  // ◻︎ 左
-};
-
-enum BinHexDec { // 数値表示タイプの列挙型(Bin, Hex, Dec)
-  Bin = 0,       // BIN
-  Hex = 1,       // HEX
-  Dec = 2,       // DEC
-};
-
-//------------------------------------------------------------------------------------
-//  クラス・構造体・共用体
-//------------------------------------------------------------------------------------
-
-// シーケンス番号理用の変数
-struct MrdSq {
-  int s_increment = 0; // フレーム毎に0-59999をカウントし, 送信
-  int r_expect = 0;    // フレーム毎に0-59999をカウントし, 受信値と比較
-};
-
-// タイマー管理用の変数
-struct MrdTimer {
-  long frame_ms = FRAME_DURATION; // 1フレームあたりの単位時間(ms)
-  int count_loop = 0;             // サイン計算用の循環カウンタ
-  int count_loop_dlt = 2;         // サイン計算用の循環カウンタを1フレームにいくつ進めるか
-  int count_loop_max = 359999;    // 循環カウンタの最大値
-  unsigned long count_frame = 0;  // メインフレームのカウント
-
-  int pad_interval = (PAD_INTERVAL - 1 > 0) ? PAD_INTERVAL - 1 : 1; // パッドの問い合わせ待機時間
-};
-
-// リモコンのアナログ入力データ
-struct PadValue {
-  unsigned short stick_R = 0;
-  int stick_R_x = 0;
-  int stick_R_y = 0;
-  unsigned short stick_L = 0;
-  int stick_L_x = 0;
-  int stick_L_y = 0;
-  unsigned short stick_L2R2V = 0;
-  int R2_val = 0;
-  int L2_val = 0;
-};
-
 // 6軸or9軸センサーの値
 struct AhrsValue {
   Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire); // BNO055のインスタンス
@@ -114,15 +36,6 @@ struct AhrsValue {
   VectorInt16 gyro;                    // [x, y, z]            角速度センサの測定値
   VectorInt16 mag;                     // [x, y, z]            磁力センサの測定値
   long temperature;                    // センサの温度測定値
-};
-
-// モニタリング設定
-struct MrdMonitor {
-  bool flow = MONITOR_FLOW;           // フローを表示
-  bool all_err = MONITOR_ERR_ALL;     // 全経路の受信エラー率を表示
-  bool servo_err = MONITOR_ERR_SERVO; // サーボエラーを表示
-  bool seq_num = MONITOR_SEQ;         // シーケンス番号チェックを表示
-  bool pad = MONITOR_PAD;             // リモコンのデータを表示
 };
 
 #endif //__MERIDIAN_MAIN_FUNC__
